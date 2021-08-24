3b7e6f04fd72cf3b52e7205ffc80dcd386f7131f
leoball <all1596315963@gmail.com>
2021-08-24 14:26:59 +0800

    第五版:歷史記錄列表-假資料的標題

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,6 +24,20 @@ namespace Homework_SkillTree.Controllers
             return View(records);
         }
 
+        public ActionResult ShowRecordTitle()
+        {
+            List<RecordTitleModel> recordTitles = new List<RecordTitleModel>();
+            string[] categoryArray = new string[] { "#", "類別", "日期", "金額" };
+            for (int i = 0; i < categoryArray.Length; i++)
+            {
+                RecordTitleModel title = new RecordTitleModel();
+                title.Title = categoryArray[i];
+                recordTitles.Add(title);
+            }
+
+            return View(recordTitles);
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
