77c6c0b6a3ac0db81b0a72adea0d10d7e388dcf6
leoball <all1596315963@gmail.com>
2021-09-22 14:35:40 +0800

    驗證金額為正整數、備註字數不超過100字

--- Models/ViewModels/CashRecordFormViewModel.cs
+++ Models/ViewModels/CashRecordFormViewModel.cs
@@ -21,6 +21,7 @@ namespace Homework_SkillTree.Models.ViewModels
 
         [Display(Name = "金額")]
         [Required(ErrorMessage ="請填寫金額!")]
+        [Range(1, Int32.MaxValue, ErrorMessage = "{0}必須為正整數!")]
         public int Money { get; set; }
 
         [Display(Name = "日期")]
@@ -29,6 +30,7 @@ namespace Homework_SkillTree.Models.ViewModels
 
         [Display(Name = "備註")]
         [Required(ErrorMessage = "請填寫備註!")]
+        [StringLength(100, ErrorMessage = "字數不得超過100字!")]
         public string Description { get; set; }
     }
 }
