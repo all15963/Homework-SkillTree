dd5dc11e1ee90dd2a7e261f3acbe4fb961b3fba0
leoball <all1596315963@gmail.com>
2021-09-22 14:33:54 +0800

    引用驗證所需js、設置表單所有欄位為必填

--- Models/ViewModels/CashRecordFormViewModel.cs
+++ Models/ViewModels/CashRecordFormViewModel.cs
@@ -16,12 +16,19 @@ namespace Homework_SkillTree.Models.ViewModels
         [Display(Name = "#")]
         public int Id { get; set; }
         [Display(Name = "類別")]
+        [Required(ErrorMessage = "請選擇類別!")]
         public string Category { get; set; }
+
         [Display(Name = "金額")]
+        [Required(ErrorMessage ="請填寫金額!")]
         public int Money { get; set; }
+
         [Display(Name = "日期")]
+        [Required(ErrorMessage = "請填寫日期!")]
         public DateTime Date { get; set; }
+
         [Display(Name = "備註")]
+        [Required(ErrorMessage = "請填寫備註!")]
         public string Description { get; set; }
     }
 }
